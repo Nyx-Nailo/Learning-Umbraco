3076a173332f8016c2ca5f2e85880c011df650aa
Fredrik Anborén <fredrik.anboren.syne22lin@edu.tucsweden.se>
2023-05-11 09:21:49 +0200

    Add project files.

---
+++ Learning.Site/Services/SearchService.cs
@@ -0,0 +1,20 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace Learning.Site.Services;
+
+public class SearchService : ISearchService
+{
+    public IEnumerable<IPublishedContent> QueryUmbraco(string searchTerm, IPublishedContent context)
+    {
+        var res = context.Root().Children.Where(x => x is NewsArticle).ToList();
+
+        return res;
+    }
+}
+
+public interface ISearchService
+{
+    IEnumerable<IPublishedContent> QueryUmbraco(string searchTerm, IPublishedContent context);
+}
